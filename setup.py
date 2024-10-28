a311096572d2ad43fc9cf8b76f81d72d0afbaf78
Jens Plüddemann <jens.plueddemann@novatec-gmbh.de>
2024-10-28 17:52:14 +0100

    updated version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='script',
-    version='1.1.0',
+    version='1.2.0',
     description='Script for syncing LDAP Users & Groups with Grafana Users & Teams',
     packages=find_packages(exclude=('tests', 'docs')),
-    package_data={'grafana-ldap-syn-script': ['run.py']},
+    package_data={'grafana-ldap-sync-script': ['run.py']},
     include_package_data=True,
     install_requires=[
         "requests>=2.24.0",
